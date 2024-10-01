89440058806167596052f95595d26ee6f73fd93e
Nilesh Shinde <nilesh.shinde@angularminds.in>
2024-10-01 12:45:43 +0530

    oct 1

--- Backend/aws/first/index.js
+++ Backend/aws/first/index.js
@@ -1,7 +1,7 @@
 // AKIARW7EP7ZIXLRPRH6F
 // "7NNzhmZMVhUL/U7Xi7guN5b3iR7OLSdXLqFo3sc1"
 
-const { GetObjectCommand, S3Client } = require('@aws-sdk/client-s3')
+const { GetObjectCommand, S3Client, PutObjectCommand, ListObjectsV2Command, DeleteObjectCommand } = require('@aws-sdk/client-s3')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 require('dotenv').config()
 
@@ -9,8 +9,7 @@ require('dotenv').config()
 const s3Client = new S3Client({
     region: "ap-south-1",
     credentials: {
-        accessKeyId: process.env.accessKeyId,
-        secretAccessKey: process.env.secretAccessKey
+
     }
 })
 
@@ -22,12 +21,56 @@ async function getObjectURL(key) {
 
     const url = await getSignedUrl(s3Client, command, { expiresIn: 20 })
     return url
+}
+
+async function putObject(filename, contentType) {
+    const command = new PutObjectCommand({
+        Bucket: "privet-buket",
+        Key: `uploads/${filename}`,
+        ContentType: contentType
+    })
+
+    const url = await getSignedUrl(s3Client, command)
+    return url
+
+
+}
+
+async function listObject(filename, contentType) {
+    const command = new ListObjectsV2Command({
+        Bucket: "privet-buket",
+        Key: `/`,
+    })
+
+    const url = await s3Client.send(command)
+    return url
+
+
+}
+
+async function deleteObject(key) {
+    const command = new DeleteObjectCommand({
+        Bucket: "privet-buket",
+        Key: key,
+    })
+
+    const url = await s3Client.send(command)
+    return url
 
 
 }
 
 async function myFunction() {
-    const url = await getObjectURL("first-img.png")
+    // const url = await getObjectURL("uploads/img-1727762543449.jpeg")
+    // console.log(url)
+
+    // const url = await putObject(`img-${Date.now()}.jpeg`, 'image/jpeg')
+    // console.log(url)
+
+    // const url = await listObject()
+    // console.log(url)
+
+    const url = await deleteObject("uploads/img-1727762543449.jpeg")
     console.log(url)
 }
 myFunction()
