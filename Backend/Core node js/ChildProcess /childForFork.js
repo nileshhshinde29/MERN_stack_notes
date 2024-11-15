901440929ec00ededba63133f06e73c6b781599e
nileshhshinde29 <91442406+nileshhshinde29@users.noreply.github.com>
2024-11-15 10:41:38 +0530

    Delete Backend/Core node js/ChildProcess  directory

--- Backend/Core node js/ChildProcess /childForFork.js
+++
@@ -1,8 +0,0 @@
-process.on('message', (message) => {
-
-    for (let i = 0; i < 10000000000; i++) {
-
-    }
-    console.log('Message from parent:', message);
-    process.send({ Hi: 'Universe' });
-});
--- Backend/Core node js/ChildProcess /fork.js
+++
@@ -1,9 +0,0 @@
-const { fork } = require('child_process');
-
-const child = fork('./childForFork.js');
-
-child.on('message', (message) => {
-    console.log('Message from child:', message);
-});
-
-child.send({ hello: 'world' });
